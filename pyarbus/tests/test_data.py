26785ca313ffb526a9cdc9bd066dc028661a6794
Paul Ivanov <pivanov314@gmail.com>
2011-10-17 13:38:48 -0700

    read short discontinuous files, too

--- pyarbus/tests/test_data.py
+++ pyarbus/tests/test_data.py
@@ -4,5 +4,11 @@ import pyarbus
 pyarbus_path = path.dirname(pyarbus.__file__)
 data_path = path.join(pyarbus_path,'data')
 def test_read_eyelink():
+    "Read a short eyelink file with discontinuous time points"
+    test_file = path.join(data_path,'pi_short.asc')
+    pyarbus.read_eyelink(test_file)
+
+def test_read_eyelink_large():
+    "Read a gunzipped .asc file (~10MB)"
     test_file = path.join(data_path,'pi.asc.gz')
     pyarbus.read_eyelink(test_file)
