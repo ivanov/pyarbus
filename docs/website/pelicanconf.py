902e4cac836f4fca4fd03868f209d2faff9314d4
Paul Ivanov <pi@berkeley.edu>
2013-10-02 16:51:06 -0700

    added basic blogging

---
+++ docs/website/pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Paul Ivanov'
+SITENAME = u'Pyarbus - eyetracking analysis software written in python.'
+SITEURL = ''
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+GITHUB_URL = 'http://github.com/ivanov/pyarbus'
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('development', 'https://github.com/ivanov/pyarbus'),
+          ('SciPy Stack', 'http://scipy.org/about.html'),
+          ('Python.org', 'http://python.org/'),
+          )
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
